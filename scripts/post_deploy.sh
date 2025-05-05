6be3c9c2f3f71f9da755ec78b86fe65695463099
GoncaloMark <gmgoncalo7@gmail.com>
2025-05-05 13:17:55 +0100

    polish the deployment scripts to use the base dir

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+set -e
 
-cd ../repos/storefront
+BASE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-# build storefront docker image
-docker build --build-arg NEXT_PUBLIC_SALEOR_API_URL=http://saleor-api.higiliquidos.svc.cluster.local/graphql/ --build-arg NEXT_PUBLIC_STOREFRONT_URL=http://store.higiliquidos.deti.com/ -t registry.deti/higiliquidos/saleor-storefront:0.1.0 --network=host --add-host saleor-api.higiliquidos.svc.cluster.local:193.136.82.35 .
+PROD_REG=registry.deti/higiliquidos
 
-docker push registry.deti/higiliquidos/saleor-storefront:0.1.0
+cd "$BASE_DIR/repos/storefront"
 
+docker build \
+  --build-arg NEXT_PUBLIC_SALEOR_API_URL=http://saleor-api.higiliquidos.svc.cluster.local/graphql/ \
+  --build-arg NEXT_PUBLIC_STOREFRONT_URL=http://store.higiliquidos.deti.com/ \
+  -t "${PROD_REG}/saleor-storefront:0.1.0" \
+  --network=host \
+  --add-host saleor-api.higiliquidos.svc.cluster.local:193.136.82.35 \
+  .
+
+docker push "${PROD_REG}/saleor-storefront:0.1.0"
+
+kubectl delete deployment storefront -n higiliquidos
+
+kustomize build "$BASE_DIR/kube/overlays/prod" | kubectl apply -n higiliquidos -f -
